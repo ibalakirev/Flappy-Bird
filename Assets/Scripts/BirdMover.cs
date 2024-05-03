b235858de1c222c3c5ace66d1806c02d831f72a5
ibalakirev <Impulseproger@gmail.com>
2024-05-04 02:31:03 +0300

    V 1.16
    Desc

---
+++ Assets/Scripts/BirdMover.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private float _tapForce;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+
+    private Vector3 _startPosition;
+    private Quaternion _maxRotation;
+    private Quaternion _minRotation;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            _rigidbody2D.velocity = new Vector2(_speed, _tapForce);
+            transform.rotation = _maxRotation;
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+    }
+}
